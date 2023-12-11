e15734120935527c342be8841a5ac524f959dee1
Peter Brian <wahomebrian98@gmail.com>
2023-12-11 07:31:58 +0300

    Merge changes from branch

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,8 @@ class BaseModel:
                         kwargs["updated_at"], self.date_format)
                 else:
                     self.__dict__[key] = kwargs[key]
-        else:git
+        else:
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
